22a0321cd0cbc6a44a824da902ac22735fcd342d
Heinz K <heka@techcollege.dk>
2019-04-10 13:36:07 +0200

    Og nu med nulstil af baggrundsfarve

--- public/js/dom-get-element.js
+++ public/js/dom-get-element.js
@@ -2,6 +2,8 @@
 let myPinkElement = document.getElementById('myPinkButton'); 
 //Henter button element med id #myGreenButton 
 let myGreenElement = document.getElementById('myGreenButton'); 
+//Henter button element med id #myResetButton 
+let myResetElement = document.getElementById('myResetButton'); 
 
 //Sætter onclick event på myPinkElement som kalder anonym funktion
 myPinkElement.onclick = function() {
@@ -13,4 +15,10 @@ myPinkElement.onclick = function() {
 myGreenElement.onclick = function() {
     //Farv baggrund (body) grøn
     document.body.style.backgroundColor = '#0F0';    
+};
+
+//Sætter onclick event på myResetElement som kalder anonym funktion
+myResetElement.onclick = function() {
+    //Nulstil baggrund
+    document.body.style.backgroundColor = 'transparent';    
 };
