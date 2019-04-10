feecc5ca968de2fdb502fc373a96581d0e2dde62
Heinz K <heka@techcollege.dk>
2019-04-10 13:33:16 +0200

    Update: Nu med kommentarer

--- public/js/dom-get-element.js
+++ public/js/dom-get-element.js
@@ -1,10 +1,16 @@
+//Henter button element med id #myPinkButton 
 let myPinkElement = document.getElementById('myPinkButton'); 
+//Henter button element med id #myGreenButton 
 let myGreenElement = document.getElementById('myGreenButton'); 
 
+//Sætter onclick event på myPinkElement som kalder anonym funktion
 myPinkElement.onclick = function() {
+    //Farv baggrund (body) lyserød
     document.body.style.backgroundColor = '#F0F';    
 };
 
+//Sætter onclick event på myGreenElement som kalder anonym funktion
 myGreenElement.onclick = function() {
+    //Farv baggrund (body) grøn
     document.body.style.backgroundColor = '#0F0';    
 };
