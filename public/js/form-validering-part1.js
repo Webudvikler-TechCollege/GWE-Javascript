96272e975e4a354f7b557fba283a0efd14950a93
Heinz K <heka@techcollege.dk>
2019-04-12 13:37:04 +0200

    Form Validering Update

--- public/js/form-validering-part1.js
+++ public/js/form-validering-part1.js
@@ -3,13 +3,35 @@
  * @param {object} form 
  */
 function validateForm(form) {
+    let result = true;
+ 
     //Hvis feltets værdi er tom...
     if(!form.firstname.value) {
         //Definer variabel med fejl meddelelse
-        let msg = "Du skal udfylde feltet";
-        //Indsæt msg efter det pågældende felts sluttag
-        form.firstname.insertAdjacentHTML('afterend', msg);
+        if(!form.firstname.nextElementSibling) {
+            let msg = '<span class="error">Du skal udfylde feltet</span>';
+            //Indsæt msg efter det pågældende felts sluttag
+            form.firstname.insertAdjacentHTML('afterend', msg);
+        }
         //Bryd ud af funktionen
+        result = false;
         return false;
+    }   
+
+    //Hvis feltets værdi er tom...
+    if(!form.lastname.value) {
+        //Definer variabel med fejl meddelelse
+        if(!form.lastname.nextElementSibling) {
+            let msg = '<span class="error">Du skal udfylde feltet</span>';
+            //Indsæt msg efter det pågældende felts sluttag
+            form.lastname.insertAdjacentHTML('afterend', msg);
+        }
+        //Bryd ud af funktionen
+        result = false;
+        return false;
+    }      
+
+    if(result) {
+        form.submit();
     }
 }
