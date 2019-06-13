cdb56bce1c4dc00874dba2ede25f3706b51c67a8
Heinz K <heka@techcollege.dk>
2019-06-13 10:21:58 +0200

    DOM Objects FOrm Validering

--- public/js/dom-objects.js
+++ public/js/dom-objects.js
@@ -1,6 +1,18 @@
 const button = document.getElementById('send');
-console.dir(button);
 
 button.onclick = function() {
-    console.dir(this);
+    //Tjek felt firstname
+    const firstname = document.getElementById('firstname');
+    if(firstname.value === '') {
+        alert('Feltet fornavn må ikke være tomt!');
+        firstname.focus();
+        return false;
+    }
+
+    //Tjek felt lastname
+    const lastname = document.getElementById('lastname');
+    if(lastname.value === '') {
+        alert('Feltet efternavn må ikke være tomt!');
+        lastname.focus();
+    }
 }
