d9f55fdd380f165ce948661ca24ad49ff164bc46
Heinz K <heka@techcollege.dk>
2019-06-13 13:26:38 +0200

    Updates

---
+++ public/js/image-slider.js
@@ -0,0 +1,39 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.onclick = () => {
+    if(counter >= carouselImages.length-1) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}
+
+prevBtn.onclick = () => {
+    if(counter <= 0) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}
+
+carouselSlide.addEventListener('transitionend', () => {
+    if(carouselImages[counter].id === 'last-clone') {
+        carouselSlide.style.transistion = 'none';
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+
+    if(carouselImages[counter].id === 'first-clone') {
+        carouselSlide.style.transistion = 'none';
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }    
+});
