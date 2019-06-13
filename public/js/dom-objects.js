0baea52bbd97dc1dd763c6232772b293961a3992
Heinz K <heka@techcollege.dk>
2019-06-13 12:33:47 +0200

    DOM Object Update

--- public/js/dom-objects.js
+++ public/js/dom-objects.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('send');
 
 button.onclick = function() {
-    //Tjek felt firstname
+    //Tjek input:text felt firstname
     const firstname = document.getElementById('firstname');
     if(firstname.value === '') {
         alert('Feltet fornavn må ikke være tomt!');
@@ -9,10 +9,29 @@ button.onclick = function() {
         return false;
     }
 
-    //Tjek felt lastname
+    //Tjek input:text felt lastname
     const lastname = document.getElementById('lastname');
-    if(lastname.value === '') {
+    if(!lastname.value) {
         alert('Feltet efternavn må ikke være tomt!');
         lastname.focus();
+        return false;
     }
+
+    //Tjek select felt gender
+    const gender = document.getElementById('gender');
+    if(gender.value === '') {
+        alert('Du skal vælge et køn!');
+        gender.focus();
+        return false;
+    }    
+
+    //Tjek input:checkbox felt accept
+    const accept = document.getElementById('accept');
+    if(!accept.checked) {
+        alert('Du skal acceptere vores betingelser!');
+        accept.focus();
+        return false;
+    }
+
+    alert('Formularen blev afsendt');
 }
