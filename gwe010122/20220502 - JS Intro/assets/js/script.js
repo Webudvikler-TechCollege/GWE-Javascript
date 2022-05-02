de589c9888532990ad52f114b2a7033dc31172ce
Heinz K <heka@techcollege.dk>
2022-05-02 10:52:03 +0200

    Eksempel med script reference

---
+++ gwe010122/20220502 - JS Intro/assets/js/script.js
@@ -0,0 +1,15 @@
+let x = document.createElement('div');
+x.setAttribute('class', 'myDiv')
+//console.dir(x);
+
+/**
+ * Multiline comment
+ */
+let firstname = "Brian"
+let lastname = "Jensen"
+
+
+// Template string
+console.log(`<p class="myClass">${firstname} ${lastname}</p>`);
+
+
