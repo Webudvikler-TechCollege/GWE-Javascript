d3ff89abd06c1a8b9fdbb73fe28e789f1dffe411
Heinz K <heka@techcollege.dk>
2022-05-03 09:35:15 +0200

    Eksempler Update

--- gwe010122/20220502 - JS Intro/assets/js/datatyper.js
+++ gwe010122/20220502 - JS Intro/assets/js/datatyper.js
@@ -1,12 +1,12 @@
 // Deklarerer en variabel med en string
-let firstname = 'Hans';
-console.log(typeof firstname);
+let username = 'Hans';
+console.log(typeof username);
 
-// Deklarerer en variabel med et heltal (number)
+// Deklarerer en variabel med et heltal el. integer (number)
 let age = 23
 console.log(typeof age);
 
-// Deklarerer en variabel med et decimaltal (number)
+// Deklarerer en variabel med et decimaltal el. float (number)
 let price = 99.95
 console.log(typeof price);
 
@@ -28,5 +28,5 @@ let car = {
 	start: () => {},
 	break: () => {}
 }
-console.log(car);
+console.log(car.model);
 
