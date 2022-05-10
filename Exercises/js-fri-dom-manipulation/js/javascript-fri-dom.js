767e17c13970a5d3db78af57dc076e9ba89ac890
Heinz K <heka@techcollege.dk>
2022-05-10 13:42:56 +0200

    Diverse update

--- Exercises/js-fri-dom-manipulation/js/javascript-fri-dom.js
+++ Exercises/js-fri-dom-manipulation/js/javascript-fri-dom.js
@@ -1,40 +1,37 @@
-/**
- * Eksempel 1
- * Tilføjer/fjerner en css klasse med større skrifttype til alle elementer
- * Benytter metoden addEventListener til at lytte på et onclick event på knappen
- */
-document.getElementById("example1").addEventListener("click", function() {
-    //Henter array med alle child elementer til #contentarea
-    let elements = document.querySelectorAll("#contentarea *");
-    //Lopper array med elementer
-    for(let i = 0; i < elements.length; i++) {
-        //Tilføj klassen tx-big hvis den ikke eksisterer i et elements classList
-        if(!elements[i].classList.contains("tx-big")) {
-            elements[i].classList.add("tx-big");
-        } else {
-            //Ellers fjern klassen hvis den eksisterer i elementets classList
-            elements[i].classList.remove("tx-big");
-        }
+const CONTENT = document.querySelector('#contentarea');
+
+// Eks
+document.getElementById('ex1').addEventListener('click', function() {
+    let headertags = CONTENT.getElementsByTagName('h1');
+    let headertag = headertags[0];
+    
+    let textnode = 'HTML Ipsum Presents';
+    if(headertag.textContent === textnode) {
+        headertag.innerText = 'Hello World!'
+    } else {
+        headertag.innerText = textnode
     }
-});
+    
+})
 
-/**
- * Eksempel 2
- * Toggler en css klasse med større skrifttype til alle elementer
- * Anvender onclick metode direkte på getElementById metoden
- * Anvender forEach metode til at loope array med
- */
-document.getElementById("example2").onclick = function() {
-    //Henter array med alle child elementer til #contentarea og kører en foreach på arrayet
-    document.querySelectorAll("#contentarea *").forEach( function(element) {
-        //Toggler class tx-big til elementernes classList
-        element.classList.toggle("tx-big");
-    });
-};
+// Eks 4
+document.getElementById('ex4').addEventListener('click', function() {
+    let h1 = CONTENT.querySelector('h1');
+    //Eksempel på inline style
+    //h1.style.display = 'none';
+    h1.classList.toggle('hide')
+})
 
-/**
- * Eksempel 3
- * Her kommer dine egne scripts til resten af knapperne...
- * 
- */
+// Eks 9
+document.getElementById('ex9').addEventListener('click', function() {
+    initLiCss('list-start-pos')
+    setTimeout(initLiCss, 20, "list-end-pos")
+})
 
+// Eks 9 - Function definition
+function initLiCss(classname) {
+    let list = CONTENT.getElementsByTagName('li');
+    for(let elm of list) {
+        elm.classList.toggle(classname)
+    }
+}
