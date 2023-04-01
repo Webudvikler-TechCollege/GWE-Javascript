09400dc731e3a5ce350c12f4185b785c57e149d2
Heinz K <heka@techcollege.dk>
2023-04-01 08:07:45 +0200

    Nu med kommentarer

--- GWE010123/codealong/modal/assets/js/modal.js
+++ GWE010123/codealong/modal/assets/js/modal.js
@@ -1,17 +1,27 @@
-const modal = document.getElementById('modal');
-const openModal = document.querySelector('#openModal');
-const closeModal = document.querySelector('#closeModal');
+// Deklarerer konstant med html element med id modal (type object)
+const modal = document.getElementById("modal")
+// Deklarerer konstant med html element med id openModal (type object)
+const openModal = document.querySelector("#openModal")
+// Deklarerer konstant med html element med id closeModal (type object)
+const closeModal = document.querySelector("#closeModal")
 
-openModal.addEventListener('click', function() {
-	modal.classList.replace('hide', 'show')
+// Tilføjer onclick event listener på knap til at åbne modal vindue med
+openModal.addEventListener("click", function () {
+  // Udskifter css klasse til at vise modalet med
+  modal.classList.replace("hide", "show")
 })
 
-closeModal.addEventListener('click', function() {
-	modal.classList.replace('show', 'hide')
+// Tilføjer onclick event listener på knap til at lukke modal vindue med
+closeModal.addEventListener("click", function () {
+  // Udskifter css klasse til at skjule modalet med
+  modal.classList.replace("show", "hide")
 })
 
-window.addEventListener('click', (event) => {
-	if(event.target.id === "modal") {
-		modal.classList.replace('show', 'hide')
-	}
+// Tilføjer event listener til browservindue (window) med event detaljer som parameter (event)
+window.addEventListener("click", (event) => {
+  // Hvis der klikkes udenfor inner_modal vinduet...
+  if (event.target.id === "modal") {
+    // Udskifter css klasse til at skjule modalet med
+    modal.classList.replace("show", "hide")
+  }
 })
